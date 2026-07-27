6acc4e45a4a39880330ca47989dfb089499021ea
Riccardo Modena <modena.riccardo@gmail.com>
2026-07-27 19:09:04 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = (
+  process.env.NEXT_PUBLIC_SUPABASE_URL || 
+  'https://aqrpkjwywepsfjdkolcr.supabase.co'
+).trim();
+
+const supabaseAnonKey = (
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxcnBrd2p3d2Vwc2ZqZGtvbGNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MzcxODcsImV4cCI6MjA5ODMxMzE4N30.z062NW4ApClll-XWHH2ufmcCleBRNHUUdKO6FiLa0TQ'
+).trim();
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);

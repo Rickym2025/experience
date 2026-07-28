7fa8efd7dd14b3e3495e006ef7f30f29759f069a
Riccardo Modena <modena.riccardo@gmail.com>
2026-07-28 15:41:47 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,20 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: '**' }
     ]
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://portale.rmstudio.app https://*.rmstudio.app;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
